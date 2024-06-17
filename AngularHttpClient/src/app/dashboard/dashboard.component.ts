fea3159290ee360aa1ab9e8a877ae0bab04886f0
Gokul Kannan <gokulrgk1110@gmail.com>
2024-06-17 19:30:34 +0530

    HttpClient all Concepts

--- AngularHttpClient/src/app/dashboard/dashboard.component.ts
+++ AngularHttpClient/src/app/dashboard/dashboard.component.ts
@@ -1,62 +1,127 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Task } from '../Model/Task';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { map, Subscription } from 'rxjs';
+import { TaskService } from '../Services/task.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit, OnDestroy {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
   allTasks: Task[] = [];
+  taskService: TaskService = new TaskService();
+  isEditMode: Boolean = false;
+  selectedTask!: Task;
+  currentTaskId: string | undefined = '';
+  isLoading: Boolean = false;
+  errMessage: string | null = null;
+  errorSub!: Subscription;
+  showDetails: Boolean = false;
+  taskId: string | undefined;
 
   ngOnInit(): void {
     this.FetchAllTasks();
+    this.errorSub = this.taskService.errorSubject.subscribe({
+      next: (httpError) => {
+        this.setErrorMessage(httpError);
+      }
+    })
   }
-  OpenCreateTaskForm(){
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
+  OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
+    this.selectedTask = new Task('', '', '', '', '', '');
   }
-  FetchTasksClick(){
+  FetchTasksClick() {
     this.FetchAllTasks();
   }
-  CloseCreateTaskForm(){
+  CloseCreateTaskForm() {
     this.showCreateTaskForm = false;
   }
-
-  CreateTask(data: Task)
-  {
+  CloseDetailsPopUp(){
+    this.showDetails = false;
+  }
+  CreateTask(data: Task) {
     // console.log('Emitted Value: ');
     // console.log(data);
-    const headers = new HttpHeaders({'my-header': 'Hello World'})
-    this.http.post<{name: string}>('https://angularhttpclient-9e62b-default-rtdb.firebaseio.com/task.json', data, {headers:headers}).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.FetchAllTasks();
-      }
-    })
+    if (this.isEditMode) {
+      this.taskService.UpdateTask(this.currentTaskId, data).subscribe({
+        next: (response) => {
+          if (response != null) {
+            this.FetchAllTasks();
+          }
+        }
+      });;
+    }
+    else {
+      this.taskService.CreateTask(data);
+    }
+
+    //console.log(this.allTasks);
   }
 
-  private FetchAllTasks()
-  {
-    this.http.get<{[key: string]: Task }>('https://angularhttpclient-9e62b-default-rtdb.firebaseio.com/task.json'      
-    ).pipe(map((response) => {
-      //Transforming Data
-      let tasks = [];
-      for(let key in response){
-        if(response.hasOwnProperty(key)){
-          tasks.push({...response[key], id: key});
-        }        
+  private FetchAllTasks() {
+    this.isLoading = true;
+    this.taskService.FetchAllTasks().subscribe({
+      next: (res) => {
+        this.allTasks = res;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+        this.setErrorMessage(err);       
       }
-      return tasks;
-    }))
-    .subscribe({
-      next: (tasks) => {
-        //console.log(tasks);
-        this.allTasks = tasks
-      }
-    })
+    });
+  }
+
+  private setErrorMessage(err: HttpErrorResponse) {
+    if (err.status === 401) {
+      this.errMessage = 'You do not have permission!'
+    }
+    else {
+      this.errMessage = err.message;
+    }
+    setTimeout(() => {
+      this.errMessage = null
+    }, 3000);
+  }
+  /**
+   * DeleteTask
+   */
+  public DeleteTask(id: string | undefined) {
+    this.taskService.DeleteTask(id);
+  }
+
+  /**
+   * name
+   */
+  public DeleteAllTasks() {
+    this.taskService.DeleteAllTasks();
+  }
+
+  OnEditTaskClicked(id: string | undefined) {
+    this.showCreateTaskForm = true;
+    this.isEditMode = true;
+    console.log(id)
+    //console.log(this.allTasks.find((task) => { return task.id === id }))
+    this.selectedTask = <Task>this.allTasks.find((task) => { return task.id === id });
+    this.currentTaskId = id;
+    console.log(this.selectedTask);
+  }
+
+  OnDetailsClicked(id: string | undefined){
+    console.log('Details Clicked!');
+    this.showDetails = true;
+    this.taskId = id;
   }
 }
