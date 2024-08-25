a3a161409bd000d20868370c765ef9ed2f7637ac
Gokul Kannan <gokulrgk1110@gmail.com>
2024-08-25 17:57:42 +0530

    missed files

--- Angular-Standalone/src/app/app-routing.module.ts
+++ Angular-Standalone/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesModule } from './courses/courses.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'courses', loadChildren: () => import('./courses/course-route.module').then(mod => mod.CourseRouteModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
