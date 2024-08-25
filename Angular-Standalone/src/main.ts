700285e64268671023c13092ffeb5546988ae831
Gokul Kannan <gokulrgk1110@gmail.com>
2024-08-25 17:58:25 +0530

    main.ts changes

--- Angular-Standalone/src/main.ts
+++ Angular-Standalone/src/main.ts
@@ -2,8 +2,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { ActionService } from './app/shared/action.service';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule } from './app/app-routing.module';
 
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [
+        ActionService,
+        importProvidersFrom(AppRoutingModule)
+    ]
+});
